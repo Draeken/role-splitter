c78e97a210737190be64030a5aace472a23a3b56
Draeken <PBM764927@gmail.com>
2020-02-03 18:19:56 +0100

    feat(client): add theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { ResponsiveTheme } from '@autoschedule/react-elements';
+import * as React from 'react';
+import * as ds from './ds-theme';
+import { css } from 'emotion';
+
+const headerClass = css`
+  display: flex;
+`;
+
+const navClass = css`
+  flex-grow: 1;
+`;
+
 const App = () => {
-  return null;
+  return (
+    <ResponsiveTheme baseTheme={ds.theme} rules={ds.rules} handleBreakpoint={ds.handleRules}>
+      <header className={headerClass}><nav className={navClass}>Home</nav></header>
+    </ResponsiveTheme>
+  );
 };
 
 export default App;
